90c20e7f87d7d729bf7509e266565428059353f8
mily rivas <mrise.translations@gmail.com>
2017-07-08 05:45:18 -0400

    fondo de bambu para el carousel de pandas

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ var anterior = $('#btn-prev');
 //mover ultima imagen al primer lugar
 $('#slider .pandas:last').insertBefore('#slider .pandas:first');
 //mostrar la primera imagen con un margen de -100%
-slider.css('margin-left', '-'+100+'%');
+slider.css('margin-left', '-'+150+'%');
 
 function moverD() {
 	slider.animate({
